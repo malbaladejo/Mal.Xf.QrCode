4b98874be660062007a39b30976318440dbed08d
malbaladejo <malbaladejo@free.fr>
2020-07-16 07:56:44 +0200

    Create project

---
+++ Mal.Xf.QrCode/Mal.Xf.QrCode/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using Mal.Xf.QrCode.Services;
+using Mal.Xf.QrCode.Views;
+
+namespace Mal.Xf.QrCode
+{
+    public partial class App : Application
+    {
+
+        public App()
+        {
+            InitializeComponent();
+
+            DependencyService.Register<MockDataStore>();
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ Mal.Xf.QrCode/Mal.Xf.QrCode/AssemblyInfo.cs
@@ -0,0 +1,8 @@
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+[assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+
+[assembly: ExportFont("FontAwesome5BrandsRegular.otf", Alias = "FontAwesomeBrands")]
+[assembly: ExportFont("FontAwesome5FreeRegular.otf", Alias = "FontAwesomeRegular")]
+[assembly: ExportFont("FontAwesome5FreeSolid.otf", Alias = "FontAwesomSolid")]
